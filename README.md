dc20f5043246c15e03969d6cbe20bb0817c89ad3
Alejandro Arenas <aarenas@pagalo.com.co>
2022-08-08 17:05:56 -0500

    Finalizacion de proyecto

--- README.md
+++ README.md
@@ -1,70 +1,46 @@
-# Getting Started with Create React App
+# Delicia Express
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+Delicia Express es un e-commerce para la venta de comidas rápidas.
 
-## Available Scripts
+Cuenta con
 
-In the project directory, you can run:
+-  filtro por categorías: tomando las categorías de la base de datos se generan filtros de productos
+- carrito de compras: cuenta vista completa de los items que vamos agregando en el carrito, su total, y la opción de sacar un producto del carrito, y en la parte derecha un resumen con el total a pagar y las opciones de finalizar compra o volver al mení
+- vistas de listas de productos: vista general de productos con foto y precio respectivo
+- vista de detalles: vista con todos los detalles del producto y la opción de agregar al carrito la cantidad que se desee cpmprar
 
-### `npm start`
+## Instalación
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+1. Clona el repositorio de github
 
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
+2. Clonado el repositorio ejecuta el comando
 
-### `npm test`
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+   npm install
 
-### `npm run build`
+    para instalar todas las dependecias del proyecto
 
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
+3. Usa
 
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
+   npm start
 
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
+    para correr el proyecto
 
-### `npm run eject`
 
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
 
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
+## Dependencias
 
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
+Uso de Bootstrap para dar formato a la aplicación.
 
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
+Uso de React Router Dom para la navegación
 
-## Learn More
+Uso de FireBase para persistencia de datos
 
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
 
-To learn React, check out the [React documentation](https://reactjs.org/).
 
-### Code Splitting
 
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
+### Autor
 
-### Analyzing the Bundle Size
+Alejandro Arenas Gómez
 
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+20212- Curso de desarrollador front-end con React
--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,32 +1,36 @@
 import "./Modal.css";
 
-import {Col, Container, Row} from 'react-bootstrap';
-import {Form, Modal} from 'react-bootstrap';
+import { Col, Container, Row } from 'react-bootstrap';
+import { Form, Modal } from 'react-bootstrap';
 import React, { useContext, useState } from "react";
 import { addDoc, collection, getFirestore } from 'firebase/firestore/lite';
 
 import CartContext from '../../store/cart-context';
 import { Link } from "react-router-dom";
+import { Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from "@mui/material";
+import CancelIcon from '@mui/icons-material/Cancel';
 
-function ModalFinalizar(props){
-    const [ nombre, setNombre ] = useState("");
-    const [ email, setEmail ] = useState("");
-    const [ telefono, setTelefono ] = useState("");
-    const [ datos, setDatos ] = useState(false);
+
+
+function ModalFinalizar(props) {
+    const [nombre, setNombre] = useState("");
+    const [email, setEmail] = useState("");
+    const [telefono, setTelefono] = useState("");
+    const [datos, setDatos] = useState(false);
     const { itemsCarrito, clearCart } = useContext(CartContext);
-    const [ nroOrden, setNroOrden ] = useState("0");
-    
-    let suma=0;
-    itemsCarrito.map( i =>  suma = ( suma + ( parseInt(i.item.precio) * parseInt(i.quality)) ));
-    
+    const [nroOrden, setNroOrden] = useState("0");
+
+    let suma = 0;
+    itemsCarrito.map(i => suma = (suma + (parseInt(i.item.precio) * parseInt(i.quality))));
+
     const db = getFirestore();
 
-    const finalizarCompra = () =>{
+    const finalizarCompra = () => {
         const arrayCompra = {
             "buyer": {
-                "name" : nombre,
-                "phone" : telefono,
-                "email" : email
+                "name": nombre,
+                "phone": telefono,
+                "email": email
             },
             "items": itemsCarrito,
             "date": Date.now(),
@@ -34,18 +38,21 @@ function ModalFinalizar(props){
         };
         try {
             addDoc(collection(db, "orders"), arrayCompra)
-            .then(({id}) => setNroOrden(id))
-            
-          } catch (e) {
+                .then(({ id }) => setNroOrden(id))
+
+        } catch (e) {
             console.error("Error adding document: ", e);
-          }
-          clearCart();
+        }
+        clearCart();
 
         setDatos(!datos);
     }
 
-    return(
-        <Modal show={props.show} onHide={props.handleClose}>
+
+
+
+    return (
+        /* <Modal show={props.show} onHide={props.handleClose}>
             <Modal.Header closeButton>
                 <Modal.Title>Finalizar compra</Modal.Title>
             </Modal.Header>
@@ -70,8 +77,8 @@ function ModalFinalizar(props){
                 <Container>
                     <Row>
                         <Col>
-                            <p className="mensajeFinal" > Muchas gracias por su compra, a la brevedad nos comunicaremos con usted!! </p>
-                            <h4>Su número de orden es: <strong style={{color:"#e12257"}}>{nroOrden}</strong></h4>
+                            <p className="mensajeFinal" >Gracias por tu compra, a la brevedad nos comunicaremos contigo. </p>
+                            <h4>Tú número de orden es: <strong style={{color:" #876445"}}>{nroOrden}</strong></h4>
                         </Col>
                     </Row>
                 </Container>
@@ -82,7 +89,56 @@ function ModalFinalizar(props){
             { datos === false ?<Link to="#" className="btn btn-lg buttonAccent float-end" onClick={finalizarCompra}> Terminar compra</Link>
             : <Link to="#" className="btn btn-lg buttonAccent float-end" onClick={props.handleClose}>Cerrar</Link>  }
             </Modal.Footer>
-        </Modal>
+        </Modal> */
+        <div>
+
+            <Dialog
+                open={props.show}
+                keepMounted
+                onClose={props.handleClose}
+                aria-describedby="alert-dialog-slide-description"
+                fullWidth
+            >
+                <section onClick={props.handleClose} className="close_button">
+                    <CancelIcon />
+                </section>
+                <DialogTitle>{"Finalizar compra"}</DialogTitle>
+                <DialogContent>
+                    {datos === false ?
+                        <Form>
+                            <Form.Group className="mb-3" controlId="Nombre">
+                                <Form.Label>Nombre y apellido</Form.Label>
+                                <Form.Control type="text" placeholder="Coloque su nombre y apellido" onChange={(event) => setNombre(event.target.value)} />
+                            </Form.Group>
+                            <Form.Group className="mb-3" controlId="Email">
+                                <Form.Label>Email</Form.Label>
+                                <Form.Control type="email" placeholder="Coloque su email" onChange={(event) => setEmail(event.target.value)} />
+                            </Form.Group>
+                            <Form.Group className="mb-3" controlId="Telefono">
+                                <Form.Label>Teléfono</Form.Label>
+                                <Form.Control type="text" placeholder="Coloque su teléfono" onChange={(event) => setTelefono(event.target.value)} />
+                            </Form.Group>
+
+                        </Form>
+                        :
+                        <Container>
+                            <Row>
+                                <Col>
+                                    <p className="mensajeFinal" >Gracias por tu compra, a la brevedad nos comunicaremos contigo. </p>
+                                    <h4>Tú número de orden es: <strong style={{ color: " #876445" }}>{nroOrden}</strong></h4>
+                                </Col>
+                            </Row>
+                        </Container>
+
+                    }
+                </DialogContent>
+                <DialogActions>
+                    {datos === false ? <Link to="#" className="btn btn-lg buttonAccent float-end" onClick={finalizarCompra}> Terminar compra</Link>
+                        : <Link to="#" className="btn btn-lg buttonAccent float-end" onClick={props.handleClose}>Cerrar</Link>}
+                </DialogActions>
+            </Dialog>
+        </div>
+
     );
 };
 export default ModalFinalizar;
